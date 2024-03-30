d8027720d5728b779dad975fc59ccad418719ffa
adamkankovsky <adamkankovsky@gmail.com>
2024-03-30 18:48:23 +0100

    Repair cnec2 corpus

--- parsers/cnec2_extended/cnec2_extended.py
+++ parsers/cnec2_extended/cnec2_extended.py
@@ -4,7 +4,7 @@ from io import BytesIO
 
 import requests
 
-from parsers.util import zip_files
+from parsers.util import zip_files, remove_files_by_extension
 
 
 def get_dataset(url_path, output_dir, dataset_name):
@@ -37,10 +37,10 @@ def get_dataset(url_path, output_dir, dataset_name):
                         annotation_parts = annotation.split("-")
                         if len(annotation_parts) == 2:
                             prefix, label = annotation_parts
-                            mapped_label = annotation_mapping.get(label, label)
+                            mapped_label = annotation_mapping.get(label, "MISC")
                             mapped_annotation = f"{prefix}-{mapped_label}"
                         else:
-                            mapped_annotation = annotation
+                            mapped_annotation = "O" if annotation == "O" else "MISC"
                         processed_lines.append(
                             "\t".join([str(line_number), word, lemma, morph, mapped_annotation]))
                     else:
@@ -54,7 +54,9 @@ def get_dataset(url_path, output_dir, dataset_name):
             with open(output_file_path, "w", encoding="utf-8") as output_file:
                 output_file.write("\n".join(processed_lines))
 
-            zip_files(output_dir, os.path.join(output_dir, f"{dataset_name}.zip"), ['.conll'])
+    zip_files(output_dir, os.path.join(output_dir, f"{dataset_name}.zip"), ['.conll'])
+
+    remove_files_by_extension(output_dir, '.conll')
 
 
 def get_cnec2_extended(url, output_dir, dataset_name):
