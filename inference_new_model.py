80ad2ab13783d1b9117eaf45bce40b246b02445c
David Chocholaty <david.chocholaty@seznam.cz>
2024-05-08 15:33:49 +0200

    Add own model loading and inference

--- inference_new_model.py
+++ inference_new_model.py
@@ -1,8 +1,28 @@
-from transformers import pipeline
+import argparse
 
-# Replace this with your own checkpoint
-model_checkpoint = "huggingface-course/bert-finetuned-ner"
-token_classifier = pipeline(
-    "token-classification", model=model_checkpoint, aggregation_strategy="simple"
-)
-token_classifier("My name is Sylvain and I work at Hugging Face in Brooklyn.")
+from transformers import pipeline
+from yaml import safe_load
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', required=True, help='Training configuration file.')
+    #parser.add_argument('--results_csv', required=True, help='Results CSV file.')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arguments()
+
+    with open(args.config, 'r') as config_file:
+        config = safe_load(config_file)
+
+    # Replace this with your own checkpoint
+    model_checkpoint = config["model"]["path"]
+    token_classifier = pipeline(
+        "token-classification", model=model_checkpoint, aggregation_strategy="simple"
+    )
+    classification_result = token_classifier("My name is Sylvain and I work at Hugging Face in Brooklyn.")
+    print(classification_result)
+
+if __name__ == "__main__":
+    main()
